b0c5e6f365ea2474b0647008d2b58661fdea6a55
YLX <ylx601@gmail.com>
2017-04-20 14:48:43 +0800

    创建工程

---
+++ app/src/main/java/com/seu601/android_usb_printer_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.seu601.android_usb_printer_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
